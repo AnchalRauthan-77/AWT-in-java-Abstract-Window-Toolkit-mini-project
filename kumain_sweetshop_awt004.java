972cab7b141d2a0b8df83bdd9b1946db85757b71
AnchalRauthan-77 <anchalanchurauthan@gmail.com>
2025-06-19 12:59:25 +0530

    Update kumain_sweetshop_awt004.java

--- kumain_sweetshop_awt004.java
+++ kumain_sweetshop_awt004.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 class kumain_sweetshop_awt004 extends Frame implements ActionListener
 {
-	Label l1,l2,l3,l4,l5,l6;
+	Label l1,l2,l3,l4,l5,l6,l7;
 	TextField tf1,tf2,tf3,tf4,tf5,tf6,tf7,tf8,tf9;
 	Checkbox cb1,cb2,cb3,cb4,cb5,cb6;
 	List list1;
@@ -10,81 +10,93 @@ class kumain_sweetshop_awt004 extends Frame implements ActionListener
 	
 	kumain_sweetshop_awt004()
 	{
+		l7=new Label("Kumain Sweet Shop");
+		l7.setBounds(100,50,500,30);
+		l7.setFont(new Font("Times new Roman",Font.BOLD,25));
+		add(l7);
+		
 		l1=new Label("Samosa");
-		l1.setBounds(50,50,100,20);
+		l1.setBounds(50,120,100,20);
+		l1.setFont(new Font("Times new Roman",Font.BOLD,15));
 		
 		l2=new Label("Chai");
-		l2.setBounds(50,80,100,20);
+		l2.setBounds(50,150,100,20);
+		l2.setFont(new Font("Times new Roman",Font.BOLD,15));
 		
 		l3=new Label("Coffee");
-		l3.setBounds(50,110,100,20);
+		l3.setBounds(50,180,100,20);
+		l3.setFont(new Font("Times new Roman",Font.BOLD,15));
 		
 		list1=new List();
 		list1.add("General");
 		list1.add("Student");
 		list1.add("Staff");
-		list1.setBounds(50,140,100,50);
+		list1.setBounds(50,220,100,50);
+		list1.setFont(new Font("Times new Roman",Font.BOLD,15));
 		
 		l4=new Label("Total");
-		l4.setBounds(190,140,100,20);
+		l4.setBounds(190,210,100,20);
+		l4.setFont(new Font("Times new Roman",Font.BOLD,15));
 		
 		l5=new Label("Discount");
-		l5.setBounds(190,170,100,20);
+		l5.setBounds(190,240,100,20);
+		l5.setFont(new Font("Times new Roman",Font.BOLD,15));
 		
 		l6=new Label("Amount");
-		l6.setBounds(190,200,100,20);
+		l6.setBounds(190,270,100,20);
+		l6.setFont(new Font("Times new Roman",Font.BOLD,15));
 		
 		tf1=new TextField(15);
-		tf1.setBounds(170,50,100,20);
+		tf1.setBounds(170,120,100,20);
 		
 		tf2=new TextField(15);
-		tf2.setBounds(170,80,100,20);
+		tf2.setBounds(170,150,100,20);
 		
 		tf3=new TextField(15);
-		tf3.setBounds(170,110,100,20);
+		tf3.setBounds(170,180,100,20);
 		
 		tf4=new TextField(15);
-		tf4.setBounds(290,50,100,20);
+		tf4.setBounds(290,120,100,20);
 	
 		cb1=new Checkbox("Chole");
-		cb1.setBounds(405,50,100,20);
+		cb1.setBounds(405,120,100,20);
 		
 		tf5=new TextField(15);
-		tf5.setBounds(290,80,100,20);
+		tf5.setBounds(290,150,100,20);
 		
 		cb2=new Checkbox("Dahi");
-		cb2.setBounds(405,80,100,20);
+		cb2.setBounds(405,150,100,20);
 		
 		tf6=new TextField(15);
-		tf6.setBounds(290,110,100,20);
+		tf6.setBounds(290,180,100,20);
 		
 		cb3=new Checkbox("Masala");
-		cb3.setBounds(405,110,100,20);
+		cb3.setBounds(405,180,100,20);
 		
 		tf7=new TextField(15);
-		tf7.setBounds(290,140,100,20);
+		tf7.setBounds(290,210,100,20);
 		
 		cb4=new Checkbox("kullad");
-		cb4.setBounds(405,140,100,20);
+		cb4.setBounds(405,210,100,20);
 		
 		tf8=new TextField(15);
-		tf8.setBounds(290,170,100,20);
+		tf8.setBounds(290,240,100,20);
 		
 		cb5=new Checkbox("Latte");
-		cb5.setBounds(405,170,100,20);
+		cb5.setBounds(405,240,100,20);
 		
 		tf9=new TextField(15);
-		tf9.setBounds(290,200,100,20);
+		tf9.setBounds(290,270,100,20);
 		
 		cb6=new Checkbox("Express");
-		cb6.setBounds(405,200,100,20);
+		cb6.setBounds(405,270,100,20);
 		
 		b1=new Button("Calculate");
-		b1.setBounds(170,230,100,20);
+		b1.setBounds(170,310,100,20);
 		b1.addActionListener(this);
 		
 		b2=new Button("Cancel");
-		b2.setBounds(290,230,100,20);
+		b2.setBounds(290,310,100,20);
 		b2.addActionListener(this);
 		
 		add(l1);
